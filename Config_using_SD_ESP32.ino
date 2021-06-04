102544163355227dbcb4c967bcfdefd30049d050
Akshay <akshaysharma280898@gmail.com>
2021-06-04 17:29:05 +0530

    updated readme and added comments in code

--- Config_using_SD_ESP32.ino
+++ Config_using_SD_ESP32.ino
@@ -1,15 +1,15 @@
 /*
    Connect the SD card to the following pins:
 
-   SD Card | ESP32
+     SD Card | ESP32
       D2       -
-      D3       SS
-      CMD      MOSI
+      D3       SS(GPIO5)
+      CMD      MOSI(GPIO23)
       VSS      GND
       VDD      3.3V
-      CLK      SCK
+      CLK      SCK(GPIO18)
       VSS      GND
-      D0       MISO
+      D0       MISO(GPIO19)
       D1       -
 */
 #include "FS.h"
@@ -24,6 +24,10 @@ char password[64];
 int address_ssid = 0;
 int address_password = sizeof(ssid) + 1;
 
+/*
+ *@brief This function reads the file and save the credentials in eeprom
+ *@param path file name that needs to be read
+ */
 void readFile(fs::FS &fs, const char * path) {
   Serial.printf("Reading file: %s\n", path);
 
@@ -66,6 +70,13 @@ void readFile(fs::FS &fs, const char * path) {
     }
   }
 }
+
+/*
+* @brief This function parses the string and save them in the variables
+* @param buff           string from which data needs to parsed
+*        WIFI_SSID      holds wifi ssid after being parsed from buff
+*        WIFI_PASSWORD  holds wifi password after being parsed from buff
+*/
 void parseAndSave(char *buff , char *WIFI_SSID, char *WIFI_PASSWORD)
 {
   char* token;
@@ -104,14 +115,16 @@ void deleteFile(fs::FS &fs, const char * path) {
 
 
 void setup() {
+  // Intialize serial communication at 115200 baud rate
   Serial.begin(115200);
+  // Intialize EEPROM with 1000 bytes size
   if (!EEPROM.begin(1000)) {
     Serial.println("Failed to initialise EEPROM");
     Serial.println("Restarting...");
     delay(1000);
     ESP.restart();
   }
-
+  // Intialize SD card 
   if (!SD.begin()) {
     Serial.println("Card Mount Failed");
     return;
@@ -137,11 +150,14 @@ void setup() {
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
   readFile(SD, "/config.txt");
+  // Read Credentils from EEPROM
   strcpy(ssid, EEPROM.readString(address_ssid).c_str());
   strcpy(password, EEPROM.readString(address_password).c_str());
+  
   Serial.println("Connecting to ");
   Serial.println(ssid);
   Serial.println(password);
+  // Intilize wifi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # ESP_SD_Config
-ESP32/ ESP8266 WiFi credentials from SD card
+
+## Introduction
+
+ESP_SD_CONFIG is an Arduino sketch that reads WiFi configuration from SD card and store them in the EEPROM of ESP module
+
+Using this sketch we don't have to hard-code WiFi credentials in the firmware. Instead we have file config.txt on an SD card, that has credentials store like this :,
+
+```json
+{
+WIFI_SSID:your_ssid,
+WIFI_PASSWORD:your_password
+}
+```
+
+At the start of the program, ESP module checks whether configuration file is present or not. If present it loads the credentials from the file and saves it in EEPROM and then deletes the file from the SD card. Then it reads the WiFi credentials from EEPROM and begins connecting to it.
+
+ 
+
+### Connection
+
+------
+
+- The Interface between the SD CARD module and  ESP32 is as follows:
+
+
+```
+     SD Card | ESP32
+      CS       SS(GPIO5)
+      CMD      MOSI(GPIO23)
+      VSS      GND
+      VDD      3.3V
+      CLK      SCK(GPIO18)
+      VSS      GND
+      D0       MISO(GPIO19)
+```
+
+- Format SD card to FAT16/FAT32.
+
+
+- Make a file in SD card as ***config.txt*** and edit the WiFi credentials in the format shown above.
+
+
+- Save the file and attach the SD card to the ESP module.
+
+
+ 
+
